6dd7693736b465b6b70d3c0af38725cf0b070c1c
aletsmc07 <alejandro.mince07@gmail.com>
2022-11-29 10:45:00 -0600

    Update files
    -Creating package.json
    -Modifying css route
    -Installing dependencies
    -Modifying error displaying school of intern

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -34,7 +34,7 @@ const generateIntern = function (intern) {
             <h3 class="card-title">${intern.name}</h3>
             <p class="card-text">${intern.id}</p>
             <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
-            <p class="github">Github: <a href="https://github.com/${intern.github}">${intern.github}</a></p>
+            <p class="github">Github: <a href="https://github.com/${intern.school}">${intern.school}</a></p>
         </div>
     </div>
     `;
@@ -80,7 +80,7 @@ const generateTeamPage = function (employeeCards) {
         <title>Team Profile</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="../dist/css/style.css">
     </head>
     <body>
         <header>
